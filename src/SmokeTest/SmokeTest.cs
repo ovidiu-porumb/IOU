ad9465c1e8635f8aa3bf42e94e58ed914cc4a3b0
Csaba Trucza <csaba.trucza@iquestgroup.com>
2014-10-30 13:36:52 +0200

    added smoketest project to main solution

---
+++ src/SmokeTest/SmokeTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace SmokeTest
+{
+    [TestFixture]
+    public class SmokeTest
+    {
+        [Test]
+        public void SmokeTestOne()
+        {
+            Assert.Fail("Oops");
+        }
+
+    }
+}
